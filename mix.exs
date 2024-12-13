b98560ffb6b92900a8238c1dd1c493573cd3a14e
Carlos Brito Lage <carlos@calmwave.ai>
2024-12-13 18:25:58 +0000

    No longer needs ssl to be running

--- mix.exs
+++ mix.exs
@@ -14,14 +14,15 @@ defmodule Jamdb.Oracle.Mixfile do
 
   def application do
     [
-      extra_applications: [:crypto, :public_key, :ssl, :xssl]
+      extra_applications: [:crypto, :public_key, :xssl]
     ]
   end
 
   defp deps do
     [
       {:ecto_sql, "~> 3.12"},
-      {:xssl, github: "calmwave-open-source/xssl", branch: "main"}
+      {:xssl,
+       github: "calmwave-open-source/xssl", ref: "390b8b933a466bb86ee13f89f3db72abd7e9308d"}
     ]
   end
 
